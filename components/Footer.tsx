9624f858fd8b731af8f66e64f2384983192824b1
Anurag Behura <behura960@gmail.com>
2025-05-14 14:09:19 +0530

    updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { Separator } from './ui/separator'
 export default function Footer() {
   return (
     <section className="w-full relative antialiased max-w-xl mx-auto px-4 sm:px-6 md:px-8 py-6 sm:py-8 md:py-12">
-      <Separator orientation='horizontal'/>
+      <Separator orientation='horizontal' className='bg-neutral-300'/>
       <span className='flex justify-center text-base font-semibold'>Thank You❤️</span>
     </section>
   )
