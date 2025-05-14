b705d8a6231b77bd06b2edb255d32e110d5602a6
Anurag Behura <behura960@gmail.com>
2025-05-14 12:34:10 +0530

    updated same color code for more better UX for ios devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "#121212" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   width: 'device-width',
   initialScale: 1,
